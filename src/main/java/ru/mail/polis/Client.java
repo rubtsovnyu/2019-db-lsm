4775852ddffdbbf8643db8560a6a3de030beb1bc
rubtsovnyu <kalimulka2@gmail.com>
2019-05-25 17:18:20 +0300

    resolve cognitive complexity

--- src/main/java/ru/mail/polis/Client.java
+++ src/main/java/ru/mail/polis/Client.java
@@ -53,8 +53,16 @@ public final class Client {
         return new String(bytes, StandardCharsets.UTF_8);
     }
 
-    private static long ttlFrom(@NotNull final String ttl) {
-        return Long.parseLong(ttl);
+    private static long ttlFrom(@NotNull final String[] tokens) {
+        long ttl = -1;
+        if (tokens.length > 3) {
+            try {
+                ttl  = Long.parseLong(tokens[3]);
+            } catch (NumberFormatException e) {
+                log.warn("wrong ttl, set to none");
+            }
+        }
+        return ttl;
     }
 
     public static void main(final String[] args) throws IOException {
@@ -87,10 +95,6 @@ public final class Client {
                 final String[] tokens = line.split(" ");
                 final String cmd = tokens[0];
                 final ByteBuffer key = ByteBuffer.wrap(tokens[1].getBytes(StandardCharsets.UTF_8));
-                long ttl = -1;
-                if (tokens.length > 3) {
-                    ttl = ttlFrom(tokens[3]);
-                }
 
                 switch (cmd) {
                     case "get":
@@ -104,7 +108,7 @@ public final class Client {
                         break;
 
                     case "put":
-                        dao.upsert(key, from(tokens[2]), ttl);
+                        dao.upsert(key, from(tokens[2]), ttlFrom(tokens));
                         break;
 
                     case "remove":
